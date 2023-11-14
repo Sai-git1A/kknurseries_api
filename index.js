8afffbd4479a63c8b9c6772126d6e67205187bb8
Sai Dharshan <saidharshan018@gmail.com>
2023-11-14 14:55:04 +0530

    Vercel Update

--- index.js
+++ index.js
@@ -8,8 +8,32 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGOURL);
+mongoose.connection.on('connected', () => {
+  console.log('database connected');
+});
+
+const plantsSchema = new schema({
+    _id: String,
+    head: Object,
+    body: Object,
+});
+
+const Plants = mongoose.model('IndoorPlants', plantsSchema, 'indoor-plants');
+
 app.get('/', (req, res) => {
     res.send('Server is running on port 3000....');
+});
+
+app.get('/indoor-plants', (req, res) => {
+    Plants.find({}, (err, plants) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(plants);
+        }
+    }); 
 });
 
 app.listen(port, () => {
